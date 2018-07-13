efe7c2efd3b1430912df6eb8b94aa88a1ca26b1b
yingmei.zhang <yingmei.zhang@yeepay.com>
2018-07-13 17:13:34 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import echarts from 'echarts'
+// import echarts from 'echarts'
 import Common from './util/util.js'
 import ElementUI from "element-ui"
 import "element-ui/lib/theme-chalk/index.css"
@@ -20,49 +20,28 @@ function getContextPath(){
 
 
 import axios from 'axios'
+import qs from 'qs'
 Vue.prototype.$echarts = echarts 
-// axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 Vue.prototype.$axios = axios
+Vue.prototype.$qs = qs
 
 // 公共路径 云上 url地址
 // axios.defaults.baseURL = 'http://47.104.99.228:8080/BusinessSys';
 // 公共路径 wzh url地址
-// axios.defaults.baseURL = 'http://172.19.41.214:8080/BusinessSys-maven';
-// axios.defaults.baseURL = 'http://10.151.30.148:8080/BusinessSys';
-// axios.defaults.baseURL = 'http://172.19.40.127:8080/BusinessSys';
-// axios.defaults.baseURL = getContextPath();
-// axios.defaults.baseURL = 'http://10.151.30.148:8888/BusinessSys-test'; 
+
 axios.defaults.baseURL = 'http://dev.fengshen.tcredit.com/BusinessSys';
-// axios.defaults.baseURL = 'http://172.19.162.41:8080/BusinessSys';
-// axios.defaults.baseURL = 'http://172.19.163.82:8080/BusinessSys';
-
-// wzh服务器公共下载地址
-// Vue.prototype.uploadBaseUrl = 'http://172.19.41.214:8080/BusinessSys-maven';
-// // 云上公共下载地址l
-// Vue.prototype.uploadBaseUrl = 'http://47.104.99.228:8080/BusinessSys';
-// 服务器公共下载地址
+// axios.defaults.baseURL = 'http://172.18.166.168:8080/BusinessSys'; //杜聪
+// axios.defaults.baseURL = 'http://172.19.41.177:8080/BusinessSys'; //刘杨涛
+// axios.defaults.baseURL = 'http://172.18.162.18:8080/BusinessSys'; //文杰
+// axios.defaults.baseURL = 'http://172.18.162.84:8066/BusinessSys';  //谷泽霖
+//axios.defaults.baseURL = 'http://10.151.30.110:8066/BusinessSys';  //服务器
+// axios.defaults.baseURL = 'http://10.151.30.110:8076/BusinessSys';  //服务器2
 // Vue.prototype.uploadBaseUrl = getContextPath();
-Vue.prototype.uploadBaseUrl = 'http://dev.fengshen.tcredit.com/BusinessSys';
-// Vue.prototype.uploadBaseUrl = 'http://172.19.40.127:8080/BusinessSys';
-// Vue.prototype.uploadBaseUrl = 'http://10.151.30.148:8888/BusinessSys-test'; 
-// Vue.prototype.uploadBaseUrl = 'http://172.19.162.41:8080/BusinessSys';
-// axios.defaults.baseURL = 'http://172.19.163.82:8080/BusinessSys';
+Vue.prototype.uploadBaseUrl = 'http://dev.fengshen.tcredit.com/BusinessSys';  //天创的上传下载  双方应该一致！！
+Vue.prototype.url = 'http://10.151.30.110:8066/BusinessSys'   // 易宝的上传下载   双方应该一致！！！ 
 
 
-// 打印的云上公共地址
-// Vue.prototype.distUrl = 'http://47.104.99.228:8080';
-// 打印的wzh公共地址
-// Vue.prototype.distUrl = 'http://172.19.41.214:8080';
-// Vue.prototype.distUrl = 'http://10.151.30.148:8888';
-
-
-
-
-
-
-// axios.defaults.baseURL = 'http://192.168.1.124:8080/BusinessSys';
-// Vue.prototype.uploadBaseUrl = 'http://192.168.1.124:8080/BusinessSys';
-// Vue.prototype.distUrl = 'http://192.168.1.120:8080';
 
 Vue.config.productionTip = false
 
