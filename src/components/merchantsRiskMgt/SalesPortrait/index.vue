53b77652d178b74d5884dad1013fa0af8fb9cbb6
王艳芳 <yanfang.wang@tcredit.com>
2018-09-10 21:11:12 +0800

    update rosterMgt/DefaultList.vue & white/index.vue

---
+++ src/components/merchantsRiskMgt/SalesPortrait/index.vue
@@ -0,0 +1,37 @@
+<!-- 销售画像 -->
+<template>
+    <div id="salesPortrait">
+        <div class="fs18 ">
+            <h3 class="dis-inline fs18">销售基本信息</h3>
+        </div>
+        <table  cellspacing="0" cellpadding="0" style="width:100%;">
+            <tr>
+                <td  class="bgf5" style="min-width:100px;">代理商编号</td>
+                <td style="min-width:100px;"></td>
+                <td  class="bgf5" style="min-width:100px;">代理商名称</td>
+                <td style="min-width:100px;"></td>
+                <td  class="bgf5" style="min-width:100px;">代理商入网日期</td>
+                <td style="min-width:100px;"></td>
+                <td class="bgf5" style="min-width:100px;">分公司</td>
+                <td style="min-width:100px;"></td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+import qs from 'qs';
+export default {
+    data() {
+
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+};
+</script>
+<style>
+
+</style>
--- src/components/rosterMgt/DefaultList.vue
+++ src/components/rosterMgt/DefaultList.vue
@@ -1,78 +1,74 @@
+<!--名单默认值配置表-->
 <template>
-  <!--名单默认值-->
-  <div>
-    <div class='listHeader' @click='refresh'>
-      <span class='headerIconRefer'></span>
-    </div>
-    <div class="dataTable clear">
-        <el-table
-        :data="tableData"
-          border
-          style="width: 100%"
-          @row-dblclick = 'dblClick'
-        >
-          <el-table-column
-            align='center'
-            v-for='item in tableDataHeader'
-            :key='item.id'
-            :prop="item.key"
-            :label="item.value"
-            width="140px"
-          >
-          </el-table-column>
-        </el-table>
-        <el-dialog title="修改名单默认值配置" :visible.sync="editListDefault" width="400px" v-dialogDrag>
-                  <el-form :model="form"  :label-position="labelPosition" label-width="100px" style='margin-left:15px'>
-                      <el-form-item label="入口类型:">
-                        <span>{{name}}</span>
-                    </el-form-item>
-                    <el-form-item label="维度默认勾选:">
-                        <el-select v-model="value5" multiple placeholder="请选择">
-                          <el-option
-                            v-for="item in optionsList"
-                            :key="item.id"
-                            :label="item.value"
-                            :value="item.key"
-                            allow-create='false'
-                            >
-                          </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="有效期:">
-                        <el-input type="number" min="1" ref='sr8' v-model="form.expiryDays" class='iptOnline' ></el-input>
-                    </el-form-item>
-                     <el-form-item label="备注:">
-                        <el-input
-                          type="textarea"
-                          :rows="2"
-                          placeholder="请输入内容"
-                          v-model="textarea"
-                          class='iptOnline'
-                          >
-                        </el-input>
-                    </el-form-item>
-                    <el-form-item label="操作员:">
-                        <span>{{modifier}}</span>
-                    </el-form-item>
-                  </el-form>
-                  <div slot="footer" class="dialog-footer">
-                    <el-button type="primary" @click='editListDefault = false'>取 消</el-button>
-                    <el-button type="primary" @click='editSubmitBtn'>确 定</el-button>
-                  </div>
-                </el-dialog>
-    </div>
-    <!-- <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page.sync="currentPage"
-      :page-sizes="[10, 20, 30, 40]"
-      :page-size="100"
-      layout="sizes, prev, pager, next"
-      :total="100">
-    </el-pagination> -->
-  </div>
+    <div>
+        <div class='listHeader' >
+            <div class="search-item">
+                <span class="search-item-label">生效场景:</span>
+                <div class="search-item-content">
+                    <el-select v-model="type" placeholder="请选择" @focus="getQueryEnum(119, 'searchTypeList')" @change="getData">
+                        <el-option
+                            v-for="item in searchTypeList"
+                            :key="item.sysconid"
+                            :label="item.sysname"
+                            :value="item.syscode">
+                        </el-option>
+                    </el-select>
+                </div>
+            </div>
 
+            <span class='headerIconRefer' @click='refresh'></span>
+        </div>
+        <div class="dataTable clear">
+            <el-table
+            :data="tableData"
+              border
+              style="width: 100%"
+              @row-dblclick='dblClick'
+            >
+              <el-table-column
+                align='center'
+                v-for='(value, key) in tableDataHeader'
+                :key='value'
+                :prop="key"
+                :label="value"
+                width="140px"
+              >
+              </el-table-column>
+            </el-table>
 
+            <el-dialog title="修改名单默认值配置" :visible.sync="editListDefault" width="400px" style="max-height: 450px; overflow-y: auto;" v-dialogDrag>
+              <el-form :model="form"  :label-position="labelPosition" label-width="100px" style='margin-left:15px'>
+                <el-form-item label="入口类型:">
+                  <span>{{form.name}}</span>
+                </el-form-item>
+                <el-form-item label="维度默认勾选:">
+                  <el-select v-model="form.latitude" multiple placeholder="请选择">
+                    <el-option
+                      v-for="(value, key) in optionsList"
+                      :key="key"
+                      :label="value"
+                      :value="key"
+                      allow-create='false'
+                      >
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="有效期:">
+                  <el-input type="number" min="1" v-model="form.expiryDays" class='iptOnline'></el-input>
+                </el-form-item>
+                <el-form-item label="备注:">
+                  <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="form.textarea" class='iptOnline'></el-input>
+                </el-form-item>
+              </el-form>
+              <div slot="footer" class="dialog-footer">
+                <el-button type="default" @click='editListDefault = false'>取 消</el-button>
+                <el-button type="primary" @click='editSubmitBtn'>确 定</el-button>
+              </div>
+            </el-dialog>
+        </div>
+
+        <!-- <Page :pageInfo="page"></Page> -->
+    </div>
 </template>
 <script>
 import qs from "qs";
@@ -82,20 +78,30 @@ export default {
       refreshPermission: false, //刷新权限
       editPermission: false, //修改权限
 
+      searchTypeList: [], //生效场景
+      type: '',
       currentPage: 1,
+      pageSize: 20,
       tableData: [],
-      tableDataHeader: [],
-      name: "",
-      form: {
-        expiryDays: 1
+      tableDataHeader: null,
+      page: {
+        isShowSizeChange: false,
+        totalCount: 0,
+        currentPage: 1,
+        pageSize: 20,
+        sizeList: [10, 20, 30, 40]
       },
       editListDefault: false,
-      optionsList: [],
-      value5: [],
-      textarea: "",
+      form: {
+        name: "",
+        editID: "",
+        latitude: [],
+        expiryDays: 1,
+        textarea: ""
+      },
+      optionsList: null,
+      latitudeLen: 0,
       labelPosition: "right",
-      modifier: "",
-      editID: ""
     };
   },
   created () {
@@ -106,105 +112,115 @@ export default {
   methods: {
     handleSizeChange() {},
     handleCurrentChange() {},
-    dblClick(row) {
-      if (this.editPermission === false) return;
-      this.name = row.entryTypeValue;
-      this.editListDefault = true;
-      this.modifier = row.modifier;
-      this.optionsList = row.list;
-      this.editID = row.id;
-      this.textarea = row.remark;
-      this.form.expiryDays = row.expiryDays;
-      if (row.detail === "") {
-        this.value5 = [];
-      } else if (row.detail !== "") {
-        this.value5 = row.detail.split(",");
-      }
+    getQueryEnum (enumType, list) {
+        this.$axios.post( "/SysConfigController/queryEnum",
+            qs.stringify({
+                sessionId: localStorage.getItem("SID"),
+                type: enumType
+            })
+        ).then(res => {
+            this[list] = res.data;
+            if (res.data && res.data.length) {
+                this.type = res.data[0].syscode;
+                this.init();
+            }
+        });
     },
     init() {
-      this.$axios
-        .get(
-          "/NameConfigController/query?sessionId=" + localStorage.getItem("SID")
-        )
-        .then(res => {
-          this.tableData = JSON.parse(res.data.data);
-          this.tableDataHeader = JSON.parse(res.data.heared);
+      this.$axios.post('/NameConfigController/query', qs.stringify({
+        type: this.type,
+        pageNum: this.currentPage,
+        pageSize: this.pageSize,
+      })).then(res => {
+        this.tableData = JSON.parse(res.data.data.result);
+        this.tableDataHeader = JSON.parse(res.data.data.header);
+
+        for (let key in this.tableDataHeader) {
+          this.latitudeLen++;
           this.tableData.forEach((ele, index) => {
-            let arr = ele.detail.split(",");
-            this.tableDataHeader.forEach((item, index) => {
-              arr.forEach(element => {
-                if (index > 0) {
-                  if (item.key === element) {
-                    let duihao = element;
-                    duihao = "✓";
-                    ele[item.key] = duihao;
-                  }
-                }
-              });
-            });
+            if (ele[key] == 'true') {
+              ele[key] = '✓';
+            } else if (ele[key] == 'false') {
+              ele[key] = '';
+            }
           });
-        })
-        .catch(error => {
-          console.log(error);
-        });
+        }
+      }).catch(error => {
+        console.log(error);
+      });
     },
     refresh() {
       this.init();
+    },
+    getData() {
+      this.tableData = [];
+      this.tableDataHeader = null;
+      this.init();
+    },
+    dblClick(row) {
+      if (this.editPermission === false) return;
+
+      this.form.name = row.entryTypeValue;
+      this.optionsList = this.tableDataHeader;
+      this.form.editID = row.id;
+      this.form.textarea = row.remark;
+      this.form.expiryDays = row.expiryDays;
+
+      if (this.latitudeLen == 0) {
+        this.form.latitude = [];
+      }
+      this.editListDefault = true;
     },
     editSubmitBtn() {
       if (this.form.expiryDays === "") {
         this.form.expiryDays = 0;
       }
 
-      this.$axios
-        .post(
-          "/NameConfigController/update",
-          qs.stringify({
-            sessionId: localStorage.getItem("SID"),
-            id: this.editID.toString(),
-            expiryDays: this.form.expiryDays,
-            remark: this.textarea,
-            detail: this.value5.join(",")
-          })
-        )
-        .then(res => {
-          if (res.data.code == 1) {
-            this.$alert(res.data.message, "系统提示", {
-              confirmButtonText: "确定",
-              type:'success',
-              callback: action => {
-                this.editListDefault = false;
-                this.init();
-              }
-            });
-          } else {
-            this.$alert(res.data.message, "系统提示", {
-              confirmButtonText: "确定",
-              type:'warning',
-              callback: action => {}
-            });
-          }
+      this.$axios.post("/NameConfigController/update",
+        qs.stringify({
+          sessionId: localStorage.getItem("SID"),
+          id: this.form.editID.toString(),
+          expiryDays: this.form.expiryDays,
+          remark: this.form.textarea,
+          detail: this.form.latitude.join(",")
         })
-        .catch(error => {
-          console.log(error);
-        });
+      ).then(res => {
+        if (res.data.code == 200) {
+          this.$alert(res.data.msg, "提示", {
+            confirmButtonText: "确定",
+            type:'success',
+            callback: action => {
+              this.editListDefault = false;
+              this.init();
+            }
+          });
+        } else {
+          this.$alert(res.data.msg, "提示", {
+            confirmButtonText: "确定",
+            type:'warning',
+            callback: action => {}
+          });
+        }
+      }).catch(error => {
+        console.log(error);
+      });
     }
   },
   mounted() {
-    this.init();
+    this.getQueryEnum(119, 'searchTypeList');
   },
   watch: {
     editListDefault() {
       if (this.editListDefault === true) {
         let arr = [];
-        this.optionsList.forEach((ele, index) => {
+        /*this.optionsList.forEach((ele, index) => {
           if (ele.sort === "1") {
             arr.push(ele.value);
           }
-        });
+        });*/
       } else if (this.editListDefault === false) {
-        this.value5 = "";
-        this.textarea = "";
+        this.form.latitude = [];
+        this.form.textarea = "";
       }
     }
   }
@@ -240,6 +256,29 @@ export default {
   float: right;
   margin-top: 20px;
   margin-right: 30px;
+}
+.search-item{
+    position: relative;
+    display: inline-block;
+    margin-top: 17px;
+    width: 33%;
+}
+.search-item .search-item-label{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 40%;
+    height: 36px;
+    line-height: 36px;
+    text-align: right;
+}
+.search-item .search-item-content{
+    margin-left: 45%;
+    width: 55%;
+    input{
+        width: 60%;
+        height: 36px;
+    }
 }
 .dataTable {
   margin-left: 10px;
