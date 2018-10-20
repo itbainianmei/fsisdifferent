02b84ac6c34cb3ec03f2aaa6db8c6044337ceebd
周研 <yan.zhou@tcredit.com>
2018-10-20 16:14:52 +0800

    pager

--- src/components/checkListMgt/DealwithOffline.vue
+++ src/components/checkListMgt/DealwithOffline.vue
@@ -524,7 +524,7 @@
                 <hr class="hr">
 
 
-                <div class="divHead">
+                <!-- <div class="divHead">
                     <span class="rideus"></span>
                      <span class='rideusText'>操作记录</span>
                     <div class="divContent" style="padding-top:0px;border:0;">
@@ -577,7 +577,7 @@
                             style="display: inline-block;float: right;margin-bottom: 15px;margin-top: 15px;">
                         </el-pagination>
                     </div>
-                </div>
+                </div> -->
             </div>
              <!--调查信息编辑  -->
             <div v-show="surveyInformationCon">
